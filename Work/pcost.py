f7e3720ef504accb895f379337ef44b13b00d0e1
Alex Okrepkyi <creating.smth@gmail.com>
2020-08-02 14:31:03 +0300

    initial commit

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+with open('Data/portfolio.csv', 'rt') as f:
+    total_cost = 0
+
+    headers = next(f)
+    print(headers)
+
+    for line in f:
+        line = line.strip().split(',')
+        current_share = int(line[1]) * float(line[-1])  # multiply total shares by share price
+        total_cost += current_share
+
+    print(f'Total cost: {total_cost}')
