a275574da140a820632f80d3c3b856c199b8eb2f
Alex Okrepkyi <creating.smth@gmail.com>
2020-08-03 21:48:09 +0300

    added list of tuples

--- Work/pcost_function.py
+++ Work/pcost_function.py
@@ -3,22 +3,21 @@ import sys
 
 def portfolio_cost(filename):
 
-    try:
-        with open(filename, 'rt') as f:
-            total_cost = 0
+    with open(filename, 'rt') as f:
+        portfolio = []
+        total_cost = 0
+        headers = next(f)
 
-            headers = next(f)
+        for line in f:
+            line = line.strip().replace('"', '').split(',')
+            company = (str(line[0]), int(line[1]), float(line[2]))
+            portfolio.append(company)
 
-            for line in f:
-                line = line.strip().split(',')
-                current_share = int(line[1]) * float(line[-1])  # multiply total shares by share price
-                total_cost += current_share
+        for title, shares, price in portfolio:
+            total_cost += shares * price
 
-            return f'Total cost: {total_cost}'
-
-    except OSError as e:
-        print('Oh no, looks like file with such a title does not exist!\n',
-              f'ErrorMessage --> "{e}"')
+        return f'Portfolio --> {portfolio}\n' \
+               f'Total cost --> {total_cost}'
 
 
 if len(sys.argv) == 2:
