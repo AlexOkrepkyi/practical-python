9e03326f627b37fc269ff0fb22414364b012b6e2
Alex Okrepkyi <creating.smth@gmail.com>
2020-08-05 21:38:53 +0300

    Structured a program as a collection of functions

--- Work/report.py
+++ Work/report.py
@@ -1,46 +1,85 @@
 # report.py
 #
 # Exercise 2.4
+from pprint import pprint
 
 
-def portfolio_cost(filename1, filename2):
-
-    with open(filename1, 'rt') as f:
-        portfolio = []
-        total_cost = 0
+def read_portfolio(filename: str) -> list:
+    """
+    Read a stock portfolio file into a list of dictionaries with the following keys:
+    name, shares, and price.
+    """
+    portfolio = []
+    with open(filename, 'rt') as f:
         skip_headers_line = next(f)
 
         for line in f:
             line = line.strip().replace('"', '').split(',')
-            company = {'name': str(line[0]), 'shares': int(line[1]), 'price': float(line[2])}
+            company = {
+                'name': str(line[0]),
+                'shares': int(line[1]),
+                'price': float(line[2])
+            }
             portfolio.append(company)
 
-        for element in portfolio:
-            total_cost += element['shares'] * element['price']
+    return portfolio
 
-    print(portfolio)
 
-    with open(filename2, 'rt') as f:
-        current_prices = []
+# pprint(read_portfolio("Data/portfolio.csv"))
+
+
+def calculate_total_cost(filename: str) -> int:
+    """
+    Calculate the total cost of the portfolio and set it to the integer
+    """
+    total_cost = 0
+    for element in read_portfolio(filename):
+        total_cost += element['shares'] * element['price']
+
+    return total_cost
+
+
+# print(f'Total cost: {calculate_total_cost("Data/portfolio.csv")}', end='\n')
+
+
+def read_current_prices(filename):
+    """
+    Read a file with current stock prices into a list of dictionaries with the following keys:
+    name, price
+    """
+    current_prices = []
+    with open(filename, 'rt') as f:
 
         for line in f:
             if len(line) > 1:
-                line = line.strip().replace('"', "").split(',')
-                company = {'name': str(line[0]), 'price': float(line[1])}
+                line = line.strip().replace('"', '').split(',')
+                company = {
+                    'name': str(line[0]),
+                    'price': float(line[1])
+                }
                 current_prices.append(company)
 
-    headers = ['Name', 'Shares', 'Price', 'Change']
+    return current_prices
 
-    print(f'{headers[0]}\t{headers[1]}\t{headers[2]}\t{headers[3]}\n'
-          f'-------\t-------\t-------\t-------')
+
+# pprint(read_current_prices("Data/prices.csv"))
+
+
+def print_report(filename_portfolio, filename_current_prices):
+    """
+    Print name, shares, price, change for the stock portfolio
+    """
+    headers = ['Name', 'Shares', 'Price', 'Change']
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}\n'
+          f'{"---------- " * len(headers)}')
 
     change = []
-    for i in current_prices:
-        for j in portfolio:
+    for i in read_current_prices(filename_current_prices):
+        for j in read_portfolio(filename_portfolio):
             if i['name'] == j['name']:
                 diff_dict = {i['name']: i['price'] - j['price']}
                 change.append(diff_dict)
-                print(f'{j["name"]}\t{j["shares"]}\t{i["price"]}\t{diff_dict[i["name"]]:0.2f}')
+                print(f'{j["name"]:>10s} {j["shares"]:>10d} {i["price"]:>10.2f} {diff_dict[i["name"]]:>10.2f}')
 
 
-print(portfolio_cost('Data/portfolio.csv', 'Data/prices.csv'))
+print(print_report('Data/portfolio.csv', 'Data/prices.csv'))
